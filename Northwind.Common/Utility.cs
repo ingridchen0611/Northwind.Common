fa95f78b4d75162bf8d1c9bebe0454ad9b2b758b
june <june0601@msn.com>
2018-10-11 11:35:41 +0800

    加入專案檔案。

---
+++ Northwind.Common/Utility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Northwind.Common
+{
+    public class Utility
+    {
+        public static string GetErrorMessage(Exception ex)
+        {
+            while (ex.InnerException != null)
+            {
+                ex = ex.InnerException;
+            }
+            return ex.Message;
+        }
+    }
+}
